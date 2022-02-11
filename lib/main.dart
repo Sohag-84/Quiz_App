a22456336230c24f9afeb141e935f7aa8855a1c6
IH Sohag <injamulhaqsohag19977@gmail.com>
2022-02-11 21:27:02 +0600

    create quiz app

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        appBar: AppBar(
+          title: const Text("Quiz app"),
+          centerTitle: true,
+          leading: const Icon(Icons.menu),
+        ),
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizApp(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizApp extends StatefulWidget {
+  const QuizApp({Key? key}) : super(key: key);
+
+  @override
+  _QuizAppState createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+
+  List<Icon> score = [
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children:  [
+        const Expanded(
+          flex: 5,
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text("This is where the question text will go",
+                  style: TextStyle(fontSize: 20,color: Colors.white),
+                ),
+              ),
+            )
+        ),
+        Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 5,left: 12,right: 12,bottom: 15),
+              child: FlatButton(
+                color: Colors.yellow,
+                child: const Text("True",
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: (){
+                 setState(() {
+                   score.add(const Icon(Icons.check,color: Colors.green,));
+                 });
+                },
+              ),
+            ),
+        ),
+        Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 5,left: 12,right: 12,bottom: 15),
+              child: FlatButton(
+                color: Colors.red,
+                child: const Text("False",
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: (){
+                  setState(() {
+                    score.add(const Icon(Icons.close,color: Colors.red,));
+                  });
+
+                },
+              ),
+            ),
+        ),
+        Row(
+          children: score
+        )
+      ],
+    );
+  }
+}
+
+
